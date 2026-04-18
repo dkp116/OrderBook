dc5476556e862a37555192a0e8e17766f12a7372
Daneel <daneel668@gmail.com>
2026-04-18 12:36:06 +0100

    #3 - added function to see if any orders need to be matched

--- include/MatchingAlgorithm.hpp
+++ include/MatchingAlgorithm.hpp
@@ -1,10 +1,13 @@
 #ifndef MATCHINGALGORITHM_HPP
 #define MATCHINGALGORITHM_HPP
 
+#include "Order.hpp"
+#include <deque>
+
 class MatchingAlgorithm{
     public:
     MatchingAlgorithm() {};
-
+    bool anyOrdersToMatch(std::deque<Order>& Bids, std::deque<Order>& Asks);
 };
 
 
