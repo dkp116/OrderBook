6aad5308d3d189b804868d5752e0a93a1614bcc6
Daneel <daneel668@gmail.com>
2026-04-19 19:48:24 +0100

    created logger classes and added to cmake

---
+++ include/ILogger.hpp
@@ -0,0 +1,10 @@
+class ILogger
+{
+public:
+    virtual ~ILogger() = default;
+
+    virtual void logTrade(double bidPrice,
+                          double askPrice,
+                          int quantity) = 0;
+};
+
