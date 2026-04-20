80a70bd237c056f6e713a98d206be11adbfa3bb0
Daneel <daneel668@gmail.com>
2026-04-20 18:15:14 +0100

    #5 - updated unit tests to include logger in the constructor of matching algorithm

--- include/MatchingAlgorithm.hpp
+++ include/MatchingAlgorithm.hpp
@@ -2,16 +2,19 @@
 #define MATCHINGALGORITHM_HPP
 
 #include "Order.hpp"
+#include "SpdlogLogger.hpp"
 #include <deque>
+#include<memory>
 
 class MatchingAlgorithm
 {
 private:
     std::deque<Order>& Bids;
     std::deque<Order>& Asks;
+    std::shared_ptr<ILogger> logger;
 
 public:
-    MatchingAlgorithm(std::deque<Order>& Bids_, std::deque<Order>& Asks_) : Bids(Bids_), Asks(Asks_) {};
+    MatchingAlgorithm(std::deque<Order>& Bids_, std::deque<Order>& Asks_ , std::shared_ptr<ILogger> logger_) : Bids(Bids_), Asks(Asks_), logger(logger_) {};
     bool anyOrdersToMatch();
     bool validateOrdersToMatch();
     void FillOrder();
