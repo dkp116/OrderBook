b94e2ddcd7dd1743e96e209dcc350d5b1dfdfcc8
Daneel <daneel668@gmail.com>
2026-04-21 17:56:35 +0100

    #9 - added id for each order

--- include/Order.hpp
+++ include/Order.hpp
@@ -1,7 +1,8 @@
 #ifndef ORDER_HPP
 #define ORDER_HPP
 
-#include <map>
+#include <atomic>
+#include <chrono>
 
 enum Type
 {
@@ -12,19 +13,28 @@ enum Type
 class Order
 {
 private:
+    static std::atomic<uint64_t> next_id;
+
     Type typeofOrder;
     int quantity_;
     double price_;
-    double time_;
+    std::chrono::high_resolution_clock::time_point time_;
+    uint64_t orderID;
 
 public:
     Order(Type typeofOrder, int quantity, double price)
-        : typeofOrder(typeofOrder), quantity_(quantity), price_(price) {}
+        : typeofOrder(typeofOrder),
+          quantity_(quantity),
+          price_(price),
+          time_(std::chrono::high_resolution_clock::now()),
+          orderID(++next_id) {}
 
     Type getType() const { return typeofOrder; }
     double getPrice() const { return price_; }
-    double getQuantity() const { return quantity_; }
-    void newQuantity(double remainingQuantity){quantity_ = remainingQuantity;}
+    int getQuantity() const { return quantity_; }
+    uint64_t getID() const { return orderID; }
+
+    void newQuantity(int remainingQuantity) { quantity_ = remainingQuantity; }
 };
 
 #endif
