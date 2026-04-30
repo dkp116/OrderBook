0794e2c9fe27b4369241c400c998491cbe116dba
Daneel <daneel668@gmail.com>
2026-04-30 16:00:17 +0100

    #14 - updated matchingalgo header

--- include/MatchingAlgorithm.hpp
+++ include/MatchingAlgorithm.hpp
@@ -12,14 +12,16 @@
 class MatchingAlgorithm
 {
 private:
-    std::multimap<double, Order, std::greater<double>>& Bids;
-    std::multimap<double, Order, std::less<double>>& Asks;
-    std::unordered_map<uint64_t, typename std::multimap<double, Order, std::greater<double>>::iterator>& bidIterators;
-    std::unordered_map<uint64_t, typename std::multimap<double, Order, std::less<double>>::iterator>& askIterators;
+    std::multimap<double, Order, std::greater<double>> &Bids;
+    std::multimap<double, Order, std::less<double>> &Asks;
+    std::unordered_map<uint64_t, typename std::multimap<double, Order, std::greater<double>>::iterator> &bidIterators;
+    std::unordered_map<uint64_t, typename std::multimap<double, Order, std::less<double>>::iterator> &askIterators;
     std::shared_ptr<ILogger> logger;
 
 public:
-    MatchingAlgorithm(std::multimap<double, Order, std::greater<double>>& Bids_, std::multimap<double, Order, std::less<double>>& Asks_ , std::unordered_map<uint64_t, typename std::multimap<double, Order, std::greater<double>>::iterator>& bidIterators_, std::unordered_map<uint64_t, typename std::multimap<double, Order, std::less<double>>::iterator>& askIterators_, std::shared_ptr<ILogger> logger_) : Bids(Bids_), Asks(Asks_), bidIterators(bidIterators_), askIterators(askIterators_), logger(logger_) {};
+    MatchingAlgorithm(std::multimap<double, Order, std::greater<double>> &Bids_, std::multimap<double, Order, std::less<double>> &Asks_, std::unordered_map<uint64_t, typename std::multimap<double, Order, std::greater<double>>::iterator> &bidIterators_, std::unordered_map<uint64_t, typename std::multimap<double, Order, std::less<double>>::iterator> &askIterators_,
+                      std::shared_ptr<ILogger> logger_) : Bids(Bids_), Asks(Asks_),
+                                                          bidIterators(bidIterators_), askIterators(askIterators_), logger(logger_) {};
     bool anyOrdersToMatch();
     bool validateOrdersToMatch();
     void FillOrder();
